ffc434a44006bdc699e9979a12119c20040b1dc9
stevedole808 <56047787+stevedole808@users.noreply.github.com>
2024-08-17 12:52:51 -1000

    added background

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import logo from './moon.png';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const imageStyle = {
+		width: '100%',
+		height: '100%',
+		objectFit: 'cover',
+	};
+
+	return (
+		<div style={{ width: '100vw', height: '100vh', overflow: 'hidden' }}>
+			<img src={logo} alt='Description' style={imageStyle} />
+		</div>
+	);
+};
 
 export default App;
